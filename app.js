6da073c1c466c39fd061626f0ca8c3a903e893c5
Daniel <daniel@personifyxp.com>
2021-12-31 15:02:56 +0000

    removal of json file

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 const cors = require("cors")
-app.use(cors())
+const { products } = require("./data")
 const PORT = 8000
 
-const db = require("./db.json")
+app.use(cors())
 
 app.listen( process.env.PORT || PORT, ()=> {
     console.log(`Server Running on ${PORT}`)
 })
 
-app.get('/', (req,res) => {
-    res.send("Hello World")
-})
 
 app.get('/products', (req,res) => {
-    res.send(db.products)
-})
-
-app.get('/categories', (req,res) => {
-    res.send(db.categories)
+    res.send(JSON.stringify(products))
 })
