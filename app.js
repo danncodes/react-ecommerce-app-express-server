c448b20ab240497b8eb2782afe1d9b7b6d372b36
Daniel <daniel@personifyxp.com>
2022-01-01 18:48:03 +0000

    stripe initial setup

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const { products } = require("./data")
+const dotenv = require('dotenv')
+dotenv.config()
 const PORT = 8000
+
+const stripe = require('stripe')('sk_test_51K7TSRJefzD4PcLRuLMAb6kiMlJ36yxlPjs9JvRKCGH4suXCiSSqAonwORcZGI7FIWSLeXcRxFyieJVkwfJhPUx200XOirlLq8');
+
 
 app.use(cors())
 
@@ -10,9 +15,34 @@ app.listen( process.env.PORT || PORT, ()=> {
     console.log(`Server Running on ${PORT}`)
 })
 
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1KDCZOJefzD4PcLRAUE5wjE0',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `https://sleepy-brahmagupta-3e1ba9.netlify.app`,
+      cancel_url: `https://sleepy-brahmagupta-3e1ba9.netlify.app/`,
+    });
+  
+    res.redirect(303, session.url);
+  });
 
 app.get('/products', (req,res) => {
     res.send(JSON.stringify(products))
 })
 
-//
+app.get('/products/:id', (req,res) => {
+    const id = req.params.id
+
+    const product = products.filter( (product) => {
+        return product.id == id
+    })
+
+    res.send(JSON.stringify(product[0]))
+
+})
